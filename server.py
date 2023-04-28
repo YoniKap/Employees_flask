4d23e4d0842780eb3b1cc75aee39615aed9e43bd
Yonatan Kaplounov <yonikas123@gmai.com>
2023-04-28 15:24:50 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Pass the required route to the decorator.
+@app.route("/test")
+def hello():
+    return "Healthy"
+
+
+@app.route("/")
+def index():
+    return "Elo Wald!!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
