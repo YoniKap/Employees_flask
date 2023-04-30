003f1efeedd0768a377883700a5627127fc7e8fc
Yonatan Kaplounov <yonikas123@gmai.com>
2023-04-30 14:33:38 +0300

    .

--- Client/Client.py
+++ Client/Client.py
@@ -1,29 +1,19 @@
 import requests
 import Employee
 isOpen=True
+url="http://127.0.0.1:5000"
 
-# requestdic = {
-#                  0: testServer(),
-#                  1: addEmployee,
-#                  2: getEmployeeByID,
-#                  3: getEmployeeByName,
-#                  4: updateEmployee,
-#                  5: deleteEmployee,
-#                  6: getAllEmployees,
-#                  7: importEmployeesFromCSV,
-#                  8: exportEmployeesFromCSV,
-#                  9: exit
-#               }
 
 def main():
   while(isOpen):
    what = int(input())
 
 def testServer():
-    print(requests.get("http://127.0.0.1:5000/test").text)
+    print(requests.get(url+"/test").text)
 
-def addEmployee(name,gender,age,id,job,salary):
-    emp = Employee(name,gender,age,id,job,salary)
+def addEmployee(name, gender, age, id, job, salary):
+    emp = Employee.Employee(name, gender, age, id, job, salary)
+    requests.post(url, data=emp.__dict__)
 
 def getEmployeeByID():
     pass
@@ -47,10 +37,21 @@ def getAllEmployees():
     pass
 
 
-
+requestdic = {
+                 0: testServer(),
+                 1: addEmployee(),
+                 2: getEmployeeByID(),
+                 3: getEmployeeByName(),
+                 4: updateEmployee(),
+                 5: deleteEmployee(),
+                 6: getAllEmployees(),
+                 7: importEmployeesFromCSV(),
+                 8: exportEmployeesFromCSV(),
+                 9: exit
+              }
 
 
 
 if __name__ == "__main__":
-    main()
 
+     addEmployee("yontan",'m',12,123123,"devops",123333)
