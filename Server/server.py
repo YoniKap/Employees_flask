b88c6c8bbc0dbf47ed1a8c50fc92c5e2d6d15851
yonatankaplounov <ypnikas123@gmail.com>
2023-05-01 20:09:43 +0300

    AddedVenv

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,30 @@
-from flask import Flask , render_template
-
+from flask import Flask , render_template , request
+import  Client
+import json
 app = Flask(__name__,template_folder="../templates")
 
 # Pass the required route to the decorator.
 @app.route("/test")
 def hello():
     return "Healthy"
+
+@app.route('/Register_employee', methods=['GET','POST'])
+
+def register_employee():
+    if request.method == 'POST':
+       Name= request.form.get('Name')
+       Gender = request.form.get('Gender')
+       Age = request.form.get('Age')
+       ID = request.form.get('ID')
+       Job = request.form.get('Job')
+       Salary = request.form.get('Salary')
+       newdata=json.dumps({"name": Name, "gender": Gender, "age": Age, "id": ID, "job": Job, "salary": Salary})
+
+       return "<center><h1>New Employee Form Was Submitted Successfully!</h1>"\
+              "<div>NAME: {} <br><br> GENDER: {} <br><br> AGE: {} <br><br> ID: {} <br><br> JOB: {} <br><br> " \
+              "SALARY :{}</div> <br> {} </center>".format(Name,Gender,Age,ID,Job,Salary,newdata)
+
+    return render_template("Register.html")
 
 
 @app.route("/", methods=['GET', 'POST'])
