d72b26be4b595ba83249f9f24019ebb3adf7c0af
yonatankaplounov <ypnikas123@gmail.com>
2023-05-02 09:57:44 +0300

    AddedVenv

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template , request
-import  Client
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import Client
 import json
 app = Flask(__name__,template_folder="../templates")
 
@@ -9,7 +10,6 @@ def hello():
     return "Healthy"
 
 @app.route('/Register_employee', methods=['GET','POST'])
-
 def register_employee():
     if request.method == 'POST':
        Name= request.form.get('Name')
@@ -18,11 +18,13 @@ def register_employee():
        ID = request.form.get('ID')
        Job = request.form.get('Job')
        Salary = request.form.get('Salary')
+       sql=f" INSERT INTO  Employees ( name, gender , age , id , job , salary) VALUES  ({Name} , {Gender} , {Age} , {ID} , {Job} , {Salary})"
        newdata=json.dumps({"name": Name, "gender": Gender, "age": Age, "id": ID, "job": Job, "salary": Salary})
 
        return "<center><h1>New Employee Form Was Submitted Successfully!</h1>"\
               "<div>NAME: {} <br><br> GENDER: {} <br><br> AGE: {} <br><br> ID: {} <br><br> JOB: {} <br><br> " \
-              "SALARY :{}</div> <br> {} </center>".format(Name,Gender,Age,ID,Job,Salary,newdata)
+              "SALARY :{}</div> <br>json: {}<br>" \
+              " SQL: {} </center>".format(Name,Gender,Age,ID,Job,Salary,newdata,sql)
 
     return render_template("Register.html")
 
@@ -34,7 +36,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-#just a test that it works
 
 
 
